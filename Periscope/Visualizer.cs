1664b1c1599f0a2b466c6dbea6832a6ff752372c
Zev Spitz <shivisi@hotmail.com>
2020-05-22 03:09:56 +0300

    Update notification chrome

--- Periscope/Visualizer.cs
+++ Periscope/Visualizer.cs
@@ -9,6 +9,7 @@ using Periscope.Debuggee;
 using static System.IO.Path;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using Octokit;
 
 namespace Periscope {
     public class Visualizer : INotifyPropertyChanged {
@@ -21,15 +22,37 @@ namespace Periscope {
             Clipboard.SetText(string.Format(formatString, rootExpression));
         });
 
+        public static void Show<TWindow, TConfig>(Type referenceType, IVisualizerObjectProvider objectProvider, IProjectInfo? projectInfo = default)
+                where TWindow : VisualizerWindowBase<TWindow, TConfig>, new()
+                where TConfig : ConfigBase<TConfig> {
+
+            Current.Initialize(referenceType, projectInfo);
+            ConfigProvider.LoadConfigFolder(referenceType);
+
+            PresentationTraceSources.DataBindingSource.Listeners.Add(new DebugTraceListener());
+
+            ConfigKey = objectProvider.GetObject() as string ?? "";
+
+            var window = new TWindow();
+            window.Initialize(objectProvider, ConfigProvider.Get<TConfig>(ConfigKey));
+            window.ShowDialog();
+        }
+
         public static Visualizer Current;
 
         static Visualizer() => Current = new Visualizer();
 
-        private string? rootExpression;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
+        private void NotifyChanged<T>(ref T current, T newValue, [CallerMemberName] string? name = null) => 
+            this.NotifyChanged(ref current, newValue, PropertyChanged, name);
+        private void NotifyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private string? rootExpression;
         public string? RootExpression {
             get => rootExpression;
-            set => this.NotifyChanged(ref rootExpression, value, PropertyChanged);
+            set => NotifyChanged(ref rootExpression, value);
         }
 
         public string? GetRootExpression() {
@@ -39,72 +62,72 @@ namespace Periscope {
             return rootExpression;
         }
 
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        private string? version;
-        public string? Version {
+        private Version? version;
+        public Version? Version {
             get => version;
-            private set => this.NotifyChanged(ref version, value, PropertyChanged);
+            private set {
+                NotifyChanged(ref version, value);
+                NotifyChanged(nameof(IsLatest));
+                NotifyChanged(nameof(VersionString));
+            }
         }
-        private string? location;
-        public string? Location {
-            get => location;
-            private set => this.NotifyChanged(ref location, value, PropertyChanged);
-        }
-        private string? filename;
-        public string? Filename {
-            get => filename;
-            private set => this.NotifyChanged(ref filename, value, PropertyChanged);
-        }
+
+        public string VersionString => $"Version: {Version}" + (IsLatest == true ? " (latest)" : "");
 
         private Version? latestVersion;
         public Version? LatestVersion {
             get => latestVersion;
-            set => this.NotifyChanged(ref latestVersion, value, PropertyChanged);
+            set {
+                NotifyChanged(ref latestVersion, value);
+                NotifyChanged(nameof(IsLatest));
+                NotifyChanged(nameof(VersionString));
+            }
         }
 
-        public (string url, string args) UrlArgs => ("explorer.exe", $"/n /e,/select,\"{location}\"");
-
-        public static string ConfigKey { get; set; } = "";
+        private string? location;
+        public string? Location {
+            get => location;
+            private set => NotifyChanged(ref location, value);
+        }
+        private string? filename;
+        public string? Filename {
+            get => filename;
+            private set => NotifyChanged(ref filename, value);
+        }
 
         private string? feedbackUrl;
         public string? FeedbackUrl {
             get => feedbackUrl;
             set {
                 if (value.IsNullOrWhitespace()) { value = null; }
-                this.NotifyChanged(ref feedbackUrl, value, PropertyChanged);
+                NotifyChanged(ref feedbackUrl, value);
             }
         }
 
-        public void LoadVersionLocationInfo(Type t) {
+        public RelayCommand? LatestVersionCheck { get; private set; }
+
+        public bool? IsLatest => 
+            Version is null || LatestVersion is null ?
+                (bool?)null :
+                Version == LatestVersion;
+
+        public (string url, string args) UrlArgs => ("explorer.exe", $"/n /e,/select,\"{location}\"");
+        public static string ConfigKey { get; set; } = "";
+
+        public void Initialize(Type t, IProjectInfo? projectInfo) {
             // This requires an externally passed type, otherwise it'll return the Periscope DLL info
             var asm = t.Assembly;
-            Version = asm.GetName().Version.ToString();
+            Version = asm.GetName().Version;
             Location = asm.Location;
             Filename = GetFileName(asm.Location);
-        }
-
-        public static void Show<TWindow, TConfig>(Type referenceType, IVisualizerObjectProvider objectProvider, IProjectInfo? projectInfo = default)
-                where TWindow : VisualizerWindowBase<TWindow, TConfig>, new()
-                where TConfig : ConfigBase<TConfig> {
 
             if (projectInfo is { }) {
-                Current.FeedbackUrl = projectInfo.FeedbackUrl;
+                FeedbackUrl = projectInfo.FeedbackUrl;
+                LatestVersionCheck = new RelayCommand(async o =>
+                    LatestVersion = await projectInfo.GetLatestVersionAsync()
+                );
+                NotifyChanged(nameof(LatestVersionCheck));
             }
-
-            Current.LoadVersionLocationInfo(referenceType);
-            ConfigProvider.LoadConfigFolder(referenceType);
-
-            PresentationTraceSources.DataBindingSource.Listeners.Add(new DebugTraceListener());
-
-            ConfigKey = objectProvider.GetObject() as string ?? "";
-
-            var window = new TWindow();
-            if (projectInfo is { }) {
-                window.Loaded += async (s, e) => Current.LatestVersion = await projectInfo.GetLatestVersionAsync();
-            }
-            window.Initialize(objectProvider, ConfigProvider.Get<TConfig>(ConfigKey));
-            window.ShowDialog();
         }
     }
 }
